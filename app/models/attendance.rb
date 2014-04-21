16f4a9bfc226d829dd677dfd33c6ef2c7702b40f
Kizashi Nagata <kizashi1122@gmail.com>
2014-04-21 08:29:03 +0000

    Modify 2 models

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,8 +1,14 @@
 class Attendance < ActiveRecord::Base
-  belongs_to :plan
-  belongs_to :user
+#  belongs_to :plan
+#  belongs_to :user
+  belongs_to :event
 
-  validates :status, presence: true
-  validates :user_id, presence: true
-  validates :plan_id, presence: true
+#  validates :status, presence: true
+#  validates :user_id, presence: true
+#  validates :plan_id, presence: true
+  validates :user_name, presence: true
+  validates :mytime, presence: true
+  validates :event_id, presence: true
+
+
 end
--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -2,16 +2,20 @@ require 'spec_helper'
 
 describe Attendance do
 
-  let(:plan) { FactoryGirl.create(:plan) }
+#  let(:plan) { FactoryGirl.create(:plan) }
   before do
-    @attendance = Attendance.new(status: 1, plan_id: plan.id, user_id: 1)
+    @attendance = Attendance.new(mytime: "1\t2\t3": 1, user_name: "aiueo", event_id: 1)
   end
 
   subject { @attendance }
 
-  it { should respond_to(:plan_id) }
-  it { should respond_to(:user_id) }
-  it { should respond_to(:status) }
+#  it { should respond_to(:plan_id) }
+#  it { should respond_to(:user_id) }
+#  it { should respond_to(:status) }
+
+  it { should respond_to(:event_id) }
+  it { should respond_to(:user_name) }
+  it { should respond_to(:mytime) }
   
   it { should be_valid }
 
@@ -20,8 +24,13 @@ describe Attendance do
     it { should_not be_valid }
   end
 
-  describe "when plan_id is not present" do
-    before { @attendance.plan_id = nil }
+  describe "when event_id is not present" do
+    before { @attendance.event_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when mytime is not present" do
+    before { @attendance.mytime = nil }
     it { should_not be_valid }
   end
 
