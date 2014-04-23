b14339fccb1d726d63957b98f9fa5f92adf7eb26
Kizashi Nagata <kizashi1122@gmail.com>
2014-04-23 06:33:54 +0000

    Fix some test code

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,21 +1,11 @@
 # -*- coding: utf-8 -*-
 FactoryGirl.define do
   factory :event, :class => Event do
-    name '絢辻さん歓迎会'
-    description <<EOS
-絢辻さん歓迎会
-場所：塚田農場 梅田阪急東通り店
-http://r.gnavi.co.jp/kagg500/
-
-EOS
-    plan_str <<EOS
-2014/1/1 19:00-
-2014/1/2 19:00-
-2014/1/3 19:00-
-2014/1/4 19:00-
-none of the above
-
-EOS
+    name         "name"
+    description  "description1\ndescription2"
+    plan_str     "2014/1/3 19:00\n2014/1/4 19:00"
+    plan         "2014/1/3 19:00\t2014/1/4 19:00"
+    url_param    "abc123abc123abc123"
   end
 end
 
--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -11,25 +11,53 @@ describe "Events" do
 
     describe "Create Event" do
       before do
-        fill_in "イベント名",        with: "Some Event"
-        fill_in "詳細", with: "Place is blah blah blah.. Url is blah blah"
-        fill_in "候補日時",        with: "1\n2\n3\n\n"
+        fill_in "イベント名", with: "Some Event"
+        fill_in "詳細",       with: "Place is blah blah blah.. \nUrl is blah blah"
+        fill_in "候補日時",   with: "1\n2\n3\n\n"
       end
 
       describe "with valid information" do
         subject { -> { click_button "イベント作成" } }
         it { should change( Event, :count ).by(1) }
-        it { should change( Plan, :count  ).by(3) }
       end
 
-      describe "with invalid information" do
+      describe "with invalid information (without name)" do
         before do
           fill_in "イベント名", with: ""
           click_button "イベント作成"
         end
         it { should have_content("error") }
       end
+
+      describe "with invalid information (without plan)" do
+        before do
+          fill_in "候補日時", with: ""
+          click_button "イベント作成"
+        end
+        it { should have_content("error") }
+      end
     end
+  end
+
+  describe "Edit Event" do
+    let(:event) { FactoryGirl.create(:event) }
+    
+    before do  
+      visit event_path(event.url_param)
+    end
+    
+    describe "page" do
+      it { should have_content('name') }
+      it { should have_content('description1') }
+      it { should have_content('description2') }
+      it { should have_content('2014/1/3 19:00') }
+      it { should have_content('2014/1/4 19:00') }
+
+      #
+      # edit operation here
+      # 
+    end
+
   end
 
 end
