b4e34238babdde9972498b30bc5e1b0a50dbab72
Kizashi Nagata <kizashi1122@gmail.com>
2014-04-23 06:15:22 +0000

    Fix a factorygirl format

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,21 +1,11 @@
 # -*- coding: utf-8 -*-
 FactoryGirl.define do
   factory :event, :class => Event do
-    name '絢辻さん歓迎会'
-    description <<EOS
-絢辻さん歓迎会
-場所：塚田農場 梅田阪急東通り店
-http://r.gnavi.co.jp/kagg500/
-
-EOS
-    plan_str <<EOS
-2014/1/1 19:00-
-2014/1/2 19:00-
-2014/1/3 19:00-
-2014/1/4 19:00-
-none of the above
-
-EOS
+    name         '絢辻さん歓迎会'
+    description  "bbb"
+    plan_str     "1\n2\n3"
+    plan         "1\t2\t3"
+    url_param    "abc123"
   end
 end
 
