08d98edb461c430d72781ebee904b1c18261dab0
Kizashi Nagata <kizashi1122@gmail.com>
2014-04-25 06:31:29 +0000

    Change pid path to test on ec2

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,8 @@
 worker_processes 2
 
-listen '/tmp/unicorn.sock'
-pid "/home/ec2-user/deploy/shared/tmp/pids/unicorn.pid"
+listen '/tmp/unicorn.sock'#
+#pid "/home/ec2-user/deploy/shared/tmp/pids/unicorn.pid"
+pid "#{shared_path}/tmp/pids/unicorn.pid"
 
 stderr_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
 stdout_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
