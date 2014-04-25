09360e3f0a7cde3a52c5828829dd31a78f66ef15
Kizashi Nagata <kizashi1122@gmail.com>
2014-04-25 03:23:20 +0000

    Use unicorn.rb instead of unicorn.conf

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,8 @@
 worker_processes 2
+
 listen '/tmp/unicorn.sock'
+pid /home/ec2-user/deploy/shared/tmp/pids
+
 stderr_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
 stdout_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
 preload_app true
