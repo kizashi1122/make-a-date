1ccfe98b75e94583e0a376f87fa669e20ef97036
Kizashi Nagata <kizashi1122@gmail.com>
2014-04-25 03:53:34 +0000

    Enclose pid path in quotes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 worker_processes 2
 
 listen '/tmp/unicorn.sock'
-pid /home/ec2-user/deploy/shared/tmp/pids
+pid "/home/ec2-user/deploy/shared/tmp/pids"
 
 stderr_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
 stdout_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
