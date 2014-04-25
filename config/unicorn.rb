ff302a4577b8d3e607482e4da2bfe8aa4f6715ed
Kizashi Nagata <kizashi1122@gmail.com>
2014-04-25 06:34:27 +0000

    Take back

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,7 @@
 worker_processes 2
 
 listen '/tmp/unicorn.sock'#
-#pid "/home/ec2-user/deploy/shared/tmp/pids/unicorn.pid"
-pid "#{shared_path}/tmp/pids/unicorn.pid"
+pid "/home/ec2-user/deploy/shared/tmp/pids/unicorn.pid"
 
 stderr_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
 stdout_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
