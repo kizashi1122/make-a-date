e3a0fe9b23cfd7b92b1c8c16f15d544f2aec968a
Kizashi Nagata <kizashi1122@gmail.com>
2014-04-28 12:30:37 +0000

    Add more tests

---
+++ spec/requests/attendances_spec.rb
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe "Attendances" do
+
+  subject { page }
+  let(:event) { FactoryGirl.create(:event) }
+
+  describe "show atenndance page" do
+    before do 
+      visit show_event_path(event.url_param)
+      click_link "あなたの予定を登録する"
+    end
+    it { should have_content("出欠を新規登録する") }
+    it { should have_selector("select", count: 2) }
+    it { should have_selector("#mytime_select_0", text: "Not OK") }
+    it { should have_selector("#mytime_select_1", text: "Not OK") }
+    it { should_not have_selector("#mytime_select_2") } 
+  end
+
+  describe "Create an attendance" do
+    let(:new_name) { "John Doe" }
+    let(:new_comment) { "I wanna participate in it." }
+
+    before do 
+      visit new_attendance_path(event.url_param)
+      fill_in "お名前", with: new_name
+      fill_in "コメント",       with: new_comment
+    end
+    
+    describe "submit with no name" do
+      before do
+        fill_in "お名前", with: ""
+        click_button "更新する" 
+      end
+      it { should have_content("エラー")}
+    end
+    
+    
+    describe "submit without re-selecting" do
+      before { click_button "更新する" }
+      it { should have_content(new_name)}
+      it { should have_content(new_comment)}
+      it { should have_selector(".glyphicon-remove-sign", count: 2) }
+    end
+
+    describe "submit with re-selecting both" do
+      before do 
+        select "Ok!",   from: "mytime_select_0"
+        select "Maybe", from: "mytime_select_1"
+        click_button "更新する"
+      end
+      it { should have_content(new_name)}
+      it { should have_content(new_comment)}
+      it { should have_selector(".glyphicon-question-sign", count: 1) }
+      it { should have_selector(".glyphicon-ok-sign", count: 1) }
+    end
+
+  end
+end
--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -10,6 +10,7 @@ describe "Events" do
     it { should have_content("イベント作成") }
 
     describe "Create Event" do
+
       before do
         fill_in "イベント名", with: "Some Event"
         fill_in "詳細",       with: "Place is blah blah blah.. \nUrl is blah blah"
@@ -17,9 +18,19 @@ describe "Events" do
       end
 
       describe "with valid information" do
-        subject { -> { click_button "イベント作成" } }
-        it { should change( Event, :count ).by(1) }
+        before { click_button "イベント作成"  }
+        it { should have_selector("h4", text: "イベント作成完了")  }
+        it { 
+          ev = Event.first
+          should have_content(ev.url_param)
+          should have_link show_event_path(ev.url_param)
+        }
       end
+
+      describe "with valid information" do
+        subject { -> { click_button "イベント作成" } }
+        it { should change( Event, :count ).by(1)  }
+       end
 
       describe "with invalid information (without name)" do
         before do
@@ -55,6 +66,21 @@ describe "Events" do
     end
   end
   
+  describe "when visiting an unexisted Event" do
+
+    let(:event) { FactoryGirl.create(:event) }
+
+    describe "show page" do
+      before  { get show_event_path("this_is_an_unexisted_url_param") } # do not use "visit". if so, response will be nil.
+      specify { expect(response).to redirect_to(root_url) }
+    end
+
+    describe "edit page" do
+      before  { get edit_event_path("this_is_an_unexisted_url_param")  }
+      specify { expect(response).to redirect_to(root_url) }
+    end
+  end
+  
   describe "Edit Event" do
     let(:event) { FactoryGirl.create(:event) }
     
@@ -69,9 +95,57 @@ describe "Events" do
       it { should have_content('description2') }
       it { should have_content('2014/1/3 19:00') }
       it { should have_content('2014/1/4 19:00') }
+
       #
       # edit operation here
       # 
+      describe "edit an existed event" do
+        let(:new_name) { "Name_Changed" }
+        let(:new_desc) { "Desc_Changed" }
+        let(:new_plan1) { "2014/1/5 19:00" }
+        let(:new_plan2) { "2014/1/6 19:00" }
+        let!(:org_plan) { event.plan }
+
+        before do
+          fill_in "イベント名", with: new_name
+          fill_in "詳細",       with: new_desc
+          fill_in "候補日時（追加）",   with: new_plan1 + "\n" + new_plan2 + "\n\n"
+        end
+
+        describe "with invalid information (without name)" do
+          before do
+            fill_in "イベント名", with: ""
+            click_button "イベント更新"
+          end
+          it { should have_content("エラー") }
+        end
+
+        describe "with invalid information (without plan)" do
+          before do
+            fill_in "候補日時（追加）", with: ""
+            click_button "イベント更新"
+          end
+          it { should have_content("エラー") }
+        end
+
+        describe "with valid information" do
+          before do
+            click_button "イベント更新"
+          end
+
+          it { should have_content(new_name) }
+          it { should have_content(new_desc) }
+          it { should have_content('2014/1/3 19:00') }
+          it { should have_content('2014/1/4 19:00') }
+          it { should have_content(new_plan1) }
+          it { should have_content(new_plan2) }
+
+          specify { expect(event.reload.name).to  eq new_name }
+          specify { expect(event.reload.description).to  eq new_desc }
+          specify { expect(event.reload.plan).to eq org_plan + "\t" + new_plan1 + "\t" + new_plan2 }
+        end
+
+      end
     end
   end
 
