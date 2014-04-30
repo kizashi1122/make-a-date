1df849d25107155bcc07fae07cf138ec59c594af
Kizashi Nagata <kizashi1122@gmail.com>
2014-04-30 10:50:05 +0000

    Use font awesome icons instead of glyphicons
    Also use ◯×△  instead of "Ok!", "Maybe" and "Not OK" on pull down.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
 module ApplicationHelper
 
   def display_status status
-    ['Not OK', 'Maybe', 'Ok!'][status]
+#    ['Not OK', 'Maybe', 'Ok!'][status]
+    ['×', '△', '◯'][status]
   end
+
 end
