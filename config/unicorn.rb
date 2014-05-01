10959f28f3c0de284fb2e0df0618543338d3af95
Kizashi Nagata <kizashi1122@gmail.com>
2014-05-01 04:29:36 +0000

    Add before_fork process which runs after executing kill -USR2
    But still doesnt work :(

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,3 +7,14 @@ stderr_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
 stdout_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
 preload_app true
 
+before_fork do |server, worker|
+  old_pid = "#{server.config[:pid]}.oldbin"
+  p "@" * 30 + old_pid
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
