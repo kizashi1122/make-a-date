d67dc9068f05ed748992d6721f5c7519d5270699
Kizashi Nagata <kizashi1122@gmail.com>
2014-05-01 03:19:21 +0000

    Move code for showing icons from helper method to partial view

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -9,24 +9,4 @@ module EventsHelper
     arr.join(DELIM)
   end
 
-  def show_status_icon status
-    icon_type = "question"
-    color = "gray"
-    title = "まだわかりません"
-
-    if status == 0
-      icon_type = "times"
-      color = "tomato"
-      title = "行けません"
-    elsif status == 1
-      # use default
-    elsif status == 2
-      icon_type = "check"
-      color = "green"
-      title = "行けます！"
-    end
-#      content_tag(:span, "", :class => "glyphicon glyphicon-" + icon_type, :style =>"color:" + color)
-    content_tag(:i, "", :class => "fa fa-" + icon_type + "-circle", :style =>"color:" + color, :title => title)
-  end
-
 end
